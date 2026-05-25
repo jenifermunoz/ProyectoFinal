c5f504982442160cda9c9368396ecebb1209e7a0
GaboGarciaUQ <gabriel.garciad@uqvirtual.edu.co>
2026-05-24 23:24:19 -0500

    Metodos de la clase Visitante y adaptaciones de algunos atributos en base a estos

--- ProyectoFinalCodigo/src/main/java/org/uniquindio/proyectofinalcodigo/model/Parque.java
+++ ProyectoFinalCodigo/src/main/java/org/uniquindio/proyectofinalcodigo/model/Parque.java
@@ -1,5 +1,7 @@
 package org.uniquindio.proyectofinalcodigo.model;
 
+import java.util.ArrayList;
+
 public class Parque {
     private String nombre;
     private String direccion;
@@ -8,6 +10,9 @@ public class Parque {
     private String paginaWeb;
     private String mapa;
     private int capacidadMax;
+    public ArrayList<Persona> listPersonas;
+    public ArrayList<Zona> listZonas;
+    public ArrayList<Atraccion> listAtracciones;
 
     public Parque(String nombre, String direccion, String clima, String nit, String paginaWeb,String mapa, int capacidadMax){
 
@@ -18,8 +23,71 @@ public class Parque {
         this.paginaWeb=paginaWeb;
         this.mapa=mapa;
         this.capacidadMax=capacidadMax;
+        this.listPersonas = new ArrayList<>();
+        this.listZonas = new ArrayList<>();
+        this.listAtracciones = new ArrayList<>();
+
     }
 
+    public ArrayList<Visitante> getListVisitantes(ArrayList<Persona> listPersonas){
+        ArrayList<Visitante> listaVisitantes = new ArrayList<>();
+        for(int i = 0; i<listPersonas.size(); i++){
+	        if(listPersonas.get(i) instanceof Visitante){
+		        listaVisitantes.add((Visitante) listPersonas.get(i));
+	        }
+        }
+        return listaVisitantes;
+    }
+
+    public ArrayList<Entrada> getListEntradas(ArrayList<Persona> listPersonas){
+        ArrayList<Entrada> listEntradas = new ArrayList<>();
+        ArrayList<Visitante> listVisitantes = getListVisitantes(listPersonas);
+        for(int i = 0; i<listVisitantes.size(); i++){
+	        for(int j = 0; i<listVisitantes.get(i).getListEntradas().size(); j++){
+		        listEntradas.add(listVisitantes.get(i).getListEntradas().get(j));
+            }
+        }
+        return listEntradas;
+    }
+
+    //Buscar Visitante con cedula
+
+    public int buscarVisitantebyCedula(String cedula){
+	    for(int i=0; i<getListVisitantes(getListPersonas()).size() ;i++){
+		    if(getListVisitantes(getListPersonas()).get(i).getCedula().equals(cedula)){
+			    return i;
+		    }
+	    }
+        return -1;
+    }
+
+    //Buscar Atraccion con nombre
+
+    public int buscarAtraccionByNombre(String nombre){
+	    for(int i=0; i<getListZonas().size(); i++){
+		    for(int j=0; j<getListZonas().get(i).getListAtracciones.size(); j++){
+			    if(getListZonas().get(i).getListAtracciones.get(j).getNombre().equalsIgnoreCase(nombre)){
+			        return j;
+			    }
+		    	return -1;
+		    }
+    	}
+    }
+
+    //Buscar Zona con nombre de una Atraccion
+
+    public int buscarZonaByNombreAtraccion(String nombre){
+    	for(int i=0; i<getListZonas().size(); i++){
+	    	for(int j=0; j<getListZonas().get(i).getListAtracciones.size(); j++){
+	    		if(getListZonas().get(i).getListAtracciones.get(j).getNombre().equalsIgnoreCase(nombre)){
+		    	    return i;
+	    		}
+			    return -1;
+		    }
+	    }
+    }   
+
+        
     public String getNombre() {
         return nombre;
     }
@@ -75,4 +143,29 @@ public class Parque {
     public void setCapacidadMax(int capacidadMax) {
         this.capacidadMax = capacidadMax;
     }
+
+    public ArrayList<Persona> getListPersonas() {
+        return listPersonas;
+    }
+
+    public void setListPersonas(ArrayList<Persona> listPersonas) {
+        this.listPersonas = listPersonas;
+    }
+
+    public ArrayList<Zona> getListZonas() {
+        return listZonas;
+    }
+
+    public void setListZonas(ArrayList<Zona> listZonas) {
+        this.listZonas = listZonas;
+    }
+
+    public ArrayList<Atraccion> getListAtracciones() {
+        return listAtracciones;
+    }
+
+    public void setListAtracciones(ArrayList<Atraccion> listAtracciones) {
+        this.listAtracciones = listAtracciones;
+    }
+
 }
--- ProyectoFinalCodigo/src/main/java/org/uniquindio/proyectofinalcodigo/model/Visitante.java
+++ ProyectoFinalCodigo/src/main/java/org/uniquindio/proyectofinalcodigo/model/Visitante.java
@@ -7,8 +7,10 @@ public class Visitante extends Persona {
     private double saldoVirtual;
     private int edad;
     private boolean fotografia;
+    private Parque theParque;
     private List<Atraccion> AtraccionesFavoritas;
-    private List<Entrada> listEntrada;
+    private List<Entrada> listEntradas;
+    private List<NotificacionAsignada> listNotificacionesAsignadas;
 
     public Visitante(double saldoVirtual, int edad, boolean fotografia, String cedula, String nombre, LocalDate fechaNacimiento, double estatura, String numeroTelefono, String nacionalidad){
 
@@ -17,9 +19,185 @@ public class Visitante extends Persona {
         this.edad=edad;
         this.fotografia=fotografia;
         this.AtraccionesFavoritas = new ArrayList<>();
-        this.listEntrada = new ArrayList<>();
+        this.listEntradas = new ArrayList<>();
+        this.listNotificacionesAsignadas = new ArrayList<>();
 
     }
+
+    //Metodos
+
+    // Inicio de Sesion - Visitante
+
+    public Visitante iniciarSesionVisitante(Parque parque, String cedula){
+	    if(parque.buscarVisitantebyCedula(cedula) != -1){
+		    Visitante visitanteactivo = parque.getListVisitantes(parque.getListPersonas()).get(parque.buscarVisitantebyCedula(cedula));
+		    return visitanteactivo;
+        }
+    }
+
+    // Visitante Activo
+
+    Visitante visitanteactivo = iniciarSesionVisitante(theParque, cedula);
+
+    // Registrar perfil de un Visitante
+
+    public boolean registrarPerfil(Parque parque, String cedula, String nombre, LocalDate fechaNacimiento, double estatura, String numeroTelefono, String nacionalidad, int edad){
+	    if(parque.buscarVisitantebyCedula(cedula) == -1){
+		    Visitante nuevovisitante = new Visitante(0, edad, false, cedula, nombre, fechaNacimiento, estatura, numeroTelefono, nacionalidad);
+		    return true;
+	    }
+	    return false;
+    }
+
+    // Actualizar perfil de un visitante
+
+    public void actualizarPerfil(String nombre, double estatura, String numeroTelefono, String nacionalidad, int edad){
+		visitanteactivo.setNombre(nombre);
+		visitanteactivo.setEstatura(estatura);
+		visitanteactivo.setNumeroTelefono(numeroTelefono);
+		visitanteactivo.setNacionalidad(nacionalidad);
+		visitanteactivo.setEdad(edad);
+    }
+
+    // Comprar tiquetes
+
+    public boolean comprarTickets(Parque parque, String cedula, int tipo){
+	    if(visitanteactivo.getListEntradas().get(visitanteactivo.getListEntradas().size()-1).isActiva() != true && parque.getCapacidadMax() > parque.getListVisitantes(parque.getListPersonas()).size()){
+		    Entrada nuevaentrada = new Entrada(null, LocalDate.now(), LocalDate.now().plusDays(5), false, null);
+		    switch(tipo){
+		    	case 1:
+			    	if(visitanteactivo.pagarConSaldoVirtual(100000)==true){
+				    	int num = 000;
+				    	for(int i=0; i<parque.getListEntradas(parque.getListPersonas()).size(); i++){
+					    	if(parque.getListEntradas(parque.getListPersonas()).get(i).getTipoEntrada() == TipoEntrada.GENERAL){
+					    		num++;
+					    	}
+					    }
+				    	nuevaentrada.setId("G"+Integer.toString(num+1));
+			    		nuevaentrada.setTipoEntrada(TipoEntrada.GENERAL);
+				    	nuevaentrada.setActiva(true);
+				    	visitanteactivo.getListEntradas().add(nuevaentrada);
+				    	return true;
+				    }
+			    	nuevaentrada = null;
+			    	return false;
+			    case 2:
+			    	if(visitanteactivo.pagarConSaldoVirtual(200000)==true){
+			    		int num = 000;
+			    		for(int i=0; i<parque.getListEntradas(parque.getListPersonas()).size(); i++){
+					    	if(parque.getListEntradas(parque.getListPersonas()).get(i).getTipoEntrada() == TipoEntrada.FAMILIAR){
+			    				num++;
+			    			}
+			    		}
+			    		nuevaentrada.setId("F"+Integer.toString(num+1));
+			    		nuevaentrada.setTipoEntrada(TipoEntrada.FAMILIAR);
+			    		nuevaentrada.setActiva(true);
+			    		visitanteactivo.getListEntradas().add(nuevaentrada);
+				    	return true;
+				    }
+			    	nuevaentrada = null;
+			    	return false;
+			    case 3:
+			    	if(visitanteactivo.pagarConSaldoVirtual(170000)==true){
+			    		int num = 000;
+			    		for(int i=0; i<parque.getListEntradas(parque.getListPersonas()).size(); i++){
+					    	if(parque.getListEntradas(parque.getListPersonas()).get(i).getTipoEntrada() == TipoEntrada.FAST_PASS){
+			    				num++;
+			    			}
+			    		}
+			    		nuevaentrada.setId("FP"+Integer.toString(num+1));
+			    		nuevaentrada.setTipoEntrada(TipoEntrada.FAST_PASS);
+			    		nuevaentrada.setActiva(true);
+			    		visitanteactivo.getListEntradas().add(nuevaentrada);
+			    		return true;
+			    	}
+			    	nuevaentrada = null;
+			    	return false;
+			    default:
+			    	nuevaentrada = null;
+			    	return false;
+		    }
+	    }
+	    return false;
+    }
+
+    //Consultar el mapa del parque
+
+    public boolean consultarMapa(Parque parque){
+	    parque.getMapa();
+
+    }
+
+    // Consultar el tiempo de espera de una atraccion
+
+    public String consultarTiemposEspera(Parque parque, String nombre){
+	    int i = parque.buscarZonaByNombreAtraccion(nombre);
+	    int j = parque.buscarAtraccionByNombre(nombre);
+	    if(i!=-1 && j!=-1){
+		    int tiempo = parque.getListZonas().get(i).getListAtracciones().get(j).getTheColaVirtual().getTiempoEspera();
+		    return tiempo + " minutos";
+	    }
+	    return "Atraccion no encontrada";
+    }
+
+    // Agregar atraccion favorita 
+
+    public boolean agregarAtraccionFavorita(Parque parque, String nombre){
+	    int i = parque.buscarZonaByNombreAtraccion(nombre);
+	    int j = parque.buscarAtraccionByNombre(nombre);
+	    if(i!=-1 && j!=-1){
+	    	visitanteactivo.getAtraccionesFavoritas().add(parque.getListZonas().get(i).getListAtracciones().get(j));
+	    	return true;
+	    }
+	    return false;
+    }
+
+    // Mostrar lista de Atracciones favoritas
+
+    public String mostrarAtraccionesFavoritas(Parque parque){
+	    String favoritas = "";
+	    for(int i=0; i<visitanteactivo.getAtraccionesFavoritas().size(); i++){
+	    	favoritas += (visitanteactivo.getAtraccionesFavoritas().get(i).toString() + "\n");
+	    }
+	    return favoritas;
+    }
+
+    // Mostrar notificaciones 
+
+    public String mostrarNotificaciones(Parque parque){
+	    String notificaciones = "";
+	    for(int i=0; i<visitanteactivo.getListNotificacionesAsignadas().size(); i++){
+	    	notificaciones += (visitanteactivo.getListNotificacionesAsignadas().get(i).getTheNotificacion().toString() + "\n");
+	    }
+	    return notificaciones;
+    }
+
+    // Unirse a la cola virtual de una Atraccion
+
+    public boolean unirseAColaVirtualAtraccion(Parque parque, Atraccion atraccion){
+		return validarAccesoCola(visitanteactivo, atraccion);
+    }
+
+    // Recargar Saldo Virtual 
+
+    public void RecargarSaldoVirtual(int cantidad){
+	    visitanteactivo.setSaldoVirtual(cantidad);
+    }
+
+    // Pagar usando Saldo Virtual
+
+    public boolean pagarConSaldoVirtual(int cantidad){
+	    if(visitanteactivo.getSaldoVirtual()>cantidad){
+		    visitanteactivo.setSaldoVirtual(visitanteactivo.getSaldoVirtual()-cantidad);
+		    return true;
+	    }
+	    return false;
+    }
+
+
+
+
+    
 
     public double getSaldoVirtual() {
         return saldoVirtual;
@@ -53,12 +231,28 @@ public class Visitante extends Persona {
         AtraccionesFavoritas = atraccionesFavoritas;
     }
 
-    public List<Entrada> getListEntrada() {
-        return listEntrada;
+    public List<Entrada> getListEntradas() {
+        return listEntradas;
     }
 
-    public void setListEntrada(List<Entrada> listEntrada) {
-        this.listEntrada = listEntrada;
+    public void setListEntradas(List<Entrada> listEntrada) {
+        this.listEntradas = listEntradas;
+    }
+
+    public Parque getTheParque() {
+        return theParque;
+    }
+
+    public void setTheParque(Parque theParque) {
+        this.theParque = theParque;
+    }
+
+    public List<NotificacionAsignada> getListNotificacionesAsignadas() {
+        return listNotificacionesAsignadas;
+    }
+
+    public void setListNotificacionesAsignadas(List<NotificacionAsignada> listNotificacionesAsignadas) {
+        this.listNotificacionesAsignadas = listNotificacionesAsignadas;
     }
 
 }
